5c91e96e628dec6dfa518aeadd78c9597e90d69e
Veronica Bustabad <vbustabad@gmail.com>
2018-09-21 01:10:50 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,60 @@
 // your code here
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+function displayRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r => '<li><a href="' + r.html_url + '">' + r.name + '</a></li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getCommits(el) {
+  const name = el.dataset.repository;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.commit.author.name +
+        '</strong>' +
+        commit.author.login +
+        ' - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+}
+
+function getBranches(el) {
+  const name = el.dataset.repository;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayBranches);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/branches');
+  req.send();
+}
+
+function displayBranches() {
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches
+   .map(
+     branch =>'<li>' + branch.name + '</li>'
+   )
+   .join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchesList;
+}
