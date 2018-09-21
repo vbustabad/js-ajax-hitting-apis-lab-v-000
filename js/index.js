ef838a993a84cb41c92757a0b4f16b3d4c667fba
Veronica Bustabad <vbustabad@gmail.com>
2018-09-21 00:30:08 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ function displayRepositories() {
 }
 
 function getCommits(el) {
-  const name = el.dataset.repo;
+  const name = el.dataset.repository;
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayCommits);
   req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
